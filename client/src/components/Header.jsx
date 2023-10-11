63667a2301aa41b4e5905ed4bb5cd99d7db78804
Chirag Bhandakkar <chiraagbhandakkar@gmail.com>
2023-10-11 12:20:03 +0530

    completed sign-up page functionality Error Handling duplicates and Redirection to sign-in page

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -15,7 +15,7 @@ export default function Header() {
           <input
             type="text"
             placeholder="Search..."
-            className="bg-transparent focus:outline-none w-25 sm:w-64"
+            className="bg-transparent focus:outline-none w-24 sm:w-64"
           />
           <FaSearch className="text-slate-600" />
         </form>
