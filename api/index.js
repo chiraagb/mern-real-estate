def5a48c067e9641894ea4977470679cc05393c6
Chirag Bhandakkar <chiraagbhandakkar@gmail.com>
2023-10-21 17:02:17 +0530

    about and pre-installation

--- api/index.js
+++ api/index.js
@@ -5,6 +5,7 @@ import authRouter from "./routes/auth.route.js";
 import listingRouter from "./routes/listing.route.js";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
+import path from "path";
 
 dotenv.config();
 
@@ -17,6 +18,7 @@ mongoose
     console.log(err);
   });
 
+const __dirname = path.resolve();
 const app = express();
 
 app.use(express.json());
@@ -34,6 +36,12 @@ app.listen(3000, () => {
 app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter); // http://localhost:3000/api/auth/signup
 app.use("/api/listing", listingRouter);
+
+app.use(express.static(path.join(__dirname, "/client/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+});
 
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,5 +1,44 @@
 import React from "react";
 
-export default function About() {
-  return <div>About</div>;
+function About() {
+  return (
+    <div className="py-20 px-40">
+      <h1 className="text-3xl font-semibold mb-4">About StellarSites</h1>
+      <p className="text-slate-700 mb-4">
+        Welcome to Stellar Sites, your trusted partner in the world of real
+        estate. We are a leading real estate agency dedicated to helping you
+        find, sell, or rent properties in the most desirable neighborhoods. Our
+        team of experienced agents is committed to providing exceptional service
+        and ensuring a seamless buying and selling process.
+      </p>
+
+      <h2 className="text-xl font-semibold mb-2">Our Mission</h2>
+      <p className="text-slate-700 mb-4">
+        At Stellar Sites, our mission is to empower our clients to achieve their
+        real estate aspirations. We achieve this through expert guidance,
+        personalized service, and an in-depth understanding of the local market.
+        Whether you're in search of your dream home, looking to sell a property,
+        or seeking to rent a space, we are here to guide you every step of the
+        way.
+      </p>
+
+      <h2 className="text-xl font-semibold mb-2">Our Expert Team</h2>
+      <p className="text-slate-700 mb-4">
+        Our team of agents brings a wealth of experience and deep knowledge of
+        the real estate industry. We are dedicated to delivering the highest
+        level of service to our valued clients. We believe that the journey of
+        buying or selling a property should be exciting and rewarding, and we
+        are fully committed to making that a reality for each and every one of
+        our clients.
+      </p>
+
+      <p className="text-slate-700">
+        Choose Stellar Sites for your real estate journey and experience
+        excellence in service, unwavering commitment, and the realization of
+        your property dreams.
+      </p>
+    </div>
+  );
 }
+
+export default About;
