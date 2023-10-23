a23aa1063803dedb34673b5c2d103b315021036a
Chirag Bhandakkar <chiraagbhandakkar@gmail.com>
2023-10-23 16:07:30 +0530

    google button UI

--- client/src/components/OAuth.jsx
+++ client/src/components/OAuth.jsx
@@ -33,12 +33,30 @@ export default function OAuth() {
     }
   };
   return (
-    <button
-      onClick={handleGoogleClick}
-      type="button"
-      className="bg-red-700 text-white p-3 rounded-lg uppercase hover:opacity-95"
-    >
-      Continue with google
-    </button>
+    <>
+      <p className="flex flex-wrap justify-center items-center p-5">
+        <hr className="w-32 h-0.5 bg-gray-950 mr-6" /> OR
+        <hr className="w-32 h-0.5 bg-gray-950 ml-6 first-letter" />
+      </p>
+      <button
+        onClick={handleGoogleClick}
+        type="button"
+        className="flex justify-center items-center p-3 bg-white rounded-lg border border-gray-300 hover:bg-slate-50"
+      >
+        <img
+          src="https://www.svgrepo.com/show/475656/google-color.svg"
+          alt="google-icon"
+          className="w-6 h-6 mr-4"
+        />
+        <span>Continue with Google</span>
+      </button>
+    </>
+    // <button
+    //   onClick={handleGoogleClick}
+    //   type="button"
+    //   className="bg-red-700 text-white p-3 rounded-lg uppercase hover:opacity-95"
+    // >
+    //   Continue with google
+    // </button>
   );
 }
